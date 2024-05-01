9bff375cc77139fd77ac6382e218b3065071a23b
f2xiao <25191617+f2xiao@users.noreply.github.com>
2024-05-01 13:12:54 -0400

    increase salt rounds and add authenticateUser function

--- controllers/user.js
+++ controllers/user.js
@@ -1,13 +1,15 @@
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const User = require('../models/user');
+const JWT_SECRET = process.env.JWT_SECRET;
 
 // Function to validate email address using regex
 const createUser = async (request, response) => {
   const { username, email, password } = request.body;
 
-  const saltRounds = 10
+  const saltRounds = 12
   const passwordHash = bcrypt.hashSync(password, saltRounds);
-
+  
   const user = new User({
     username,
     email,
@@ -20,6 +22,36 @@ const createUser = async (request, response) => {
 
 }
 
+const authenticateUser = async (req, res) => {
+  // Get username and password from req.body
+  const { username, password } = req.body;
+
+
+  // Check if the user exists (if not, return 404)
+  const user = await User.findOne({username});
+
+  // If the user exists, check if their password is correct (if not, return 401)
+  const isPasswordCorrect = user === null ? false : bcrypt.compareSync(password, user.passwordHash);
+  if (!isPasswordCorrect) {
+    return res.status(401).json({error: "Invalid username or passsword"});
+  }
+
+  // The user auth checks out, return a signed token (with user info)
+  const authToken = jwt.sign(
+    {
+      user_id: user._id,
+      username: user.username
+    },
+    JWT_SECRET,
+    { expiresIn: "2d" }
+  );
+
+  // Send the token back to the user
+  res.status(200).json({ token: authToken });
+}
+
 module.exports = {
-  createUser
+  createUser,
+  authenticateUser
 };
+
